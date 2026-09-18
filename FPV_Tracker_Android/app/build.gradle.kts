6e5f94559afc51b581754fe7c8e159e523a98f20
arcanalord <30724643+arcanalord@users.noreply.github.com>
2026-09-19 01:04:19 +0400

    Use stable Android API 36

--- FPV_Tracker_Android/app/build.gradle.kts
+++ FPV_Tracker_Android/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "ru.fpvclub.tracker"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.fpvclub.tracker"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 3
         versionName = "0.3.0"
     }
