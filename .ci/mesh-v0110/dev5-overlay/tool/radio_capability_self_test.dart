62952cfa655d36a7f67e13a83dfa9f196239ca4a
arcanalord <30724643+arcanalord@users.noreply.github.com>
2026-09-25 20:42:45 +0400

    mesh flutter dev10: add radio_capability_self_test.dart

---
+++ .ci/mesh-v0110/dev5-overlay/tool/radio_capability_self_test.dart
@@ -0,0 +1,25 @@
+import '../lib/platform/radio_capability_contract.dart';
+
+void main() {
+  final sx = RadioInfo.fromJson({'radioFamily': 'SX1280', 'boardId': 'ep2'});
+  final caps = RadioCapabilities.fromJson({
+    'radioFamily': 'SX1280',
+    'profileIds': ['A', 'A'],
+    'networkProtocols': ['MMRP/1'],
+    'frequencyRanges': [
+      [2500, 2400],
+      [2400, 2500],
+    ],
+    'rssiAvailable': true,
+    'rangingAvailable': false,
+    'maxPayload': 512,
+  }, info: sx);
+  if (caps.rangingAvailable) throw StateError('ranging inferred incorrectly');
+  if (!caps.rssiAvailable || !caps.supportsMmrp) {
+    throw StateError('capability normalization failed');
+  }
+  if (caps.profileIds.length != 1 || caps.frequencyRanges.length != 1) {
+    throw StateError('capability sanitization failed');
+  }
+  print('RADIO_CAPABILITY_DART_PASS');
+}
